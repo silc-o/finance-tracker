e86c17aa26fe98b5885b2f524c8778b6ab8a9e65
silc-o <bhussienaldondie@gmail.com>
2026-03-13 16:29:43 +0800

    setup config file for reading .env file

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,4 +2,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    SEC
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
